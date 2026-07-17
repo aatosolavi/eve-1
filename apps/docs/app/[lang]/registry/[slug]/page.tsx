1dded30cf0181b851b200ae1ae6dccf4495dd26e
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2026-07-17 14:58:16 -0700

    docs: remove unused registry redirects
    Signed-off-by: Allen Zhou <46854522+allenzhou101@users.noreply.github.com>

--- apps/docs/app/[lang]/registry/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-
-const RegistryDetailPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
-  const { slug } = await params;
-  permanentRedirect(`/templates/${slug}`);
-};
-
-export default RegistryDetailPage;
--- apps/docs/app/[lang]/registry/page.tsx
+++
@@ -1,5 +0,0 @@
-import { permanentRedirect } from "next/navigation";
-
-const RegistryPage = () => permanentRedirect("/templates");
-
-export default RegistryPage;
